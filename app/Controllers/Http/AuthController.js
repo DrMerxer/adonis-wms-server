e66600189d944c75d51bfbe10253b4fdeaa418f0
DrMerxer <drmercer@163.com>
2019-04-14 19:48:16 +0800

    Login Success!

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -4,6 +4,15 @@ class AuthController {
    async login({ view }) {
       return view.render('auth.login')
    }
+
+   async auth ({ request, response, auth }) {
+      const { uid, password } = request.all()
+
+      await auth.attempt(uid, password)
+
+      const user = await auth.getUser()
+      return response.route('UserController.show', {id: user.id})
+   }
 }
 
 module.exports = AuthController
