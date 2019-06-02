607fd1daaf1ba0c117dfb00147fccc81ae2df949
abnernat <abnernat@abnernatdeMacBook-Pro-14.local>
2019-06-02 09:34:18 +0800

    factory migration

--- database/factory.js
+++ database/factory.js
@@ -16,17 +16,58 @@
 const Factory = use('Factory')
 const Hash = use('Hash')
 
-Factory.blueprint('App/Models/User', async (faker) => {
-  return {
-    username: faker.username(),
-    email: faker.email(),
-    password: await Hash.make(faker.password()),
-    gender:true,
-    isdel:false,
-    level:1,
-    birth:Date()
-  }
-})
+const User = use('App/Model/User')
+const Cargo = use('App/Model/Cargo')
+const Finance = use('App/Model/Finance')
+const Merchant = use('App/Model/Merchant')
+const Order = use('App/Model/Order')
+const Rfid = use('App/Model/Rfid')
+const Shelf = use('App/Model/Shelf')
+const Ship = use('App/Model/Ship')
+const Token = use('App/Model/Token')
+const User = use('App/Model/User')
+const Warehouse = use('App/Model/Warehouse')
+
+await Factory
+  .model(User)
+  .createMany(5)
+await Factory
+  .model(Token)
+  .createMany(5)
+await Factory
+  .model(Rfid)
+  .createMany(5)
+await Factory
+  .model(Merchant)
+  .createMany(5)
+await Factory
+  .model(Warehouse)
+  .createMany(5)
+await Factory
+  .model(Shelf)
+  .createMany(5)
+await Factory
+  .model(Order)
+  .createMany(5)
+await Factory
+  .model(Ship)
+  .createMany(5)
+await Factory
+  .model(Cargo)
+  .createMany(5)
+await Factory
+  .model(Finance)
+  .createMany(5)
 
 
-
+// Factory.blueprint('App/Models/User', async (faker) => {
+//   return {
+//     username: faker.username(),
+//     email: faker.email(),
+//     password: await Hash.make(faker.password()),
+//     gender:true,
+//     isdel:false,
+//     level:1,
+//     birth:Date()
+//   }
+// })
